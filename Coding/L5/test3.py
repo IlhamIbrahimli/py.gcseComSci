5afacd0534e30fee9051e3b383f6b8ab0b248c6d
Ilham Ibrahimli <78649705+IlhamIbrahimli@users.noreply.github.com>
2023-11-14 11:56:57 +0000

    Create test3.py

---
+++ Coding/L5/test3.py
@@ -0,0 +1,9 @@
+inp = input("Enter A B or C: ")
+if inp=="A":
+    print(f'You chose {inp}!')
+elif inp=="B":
+    print(f'You chose {inp}!')
+elif inp=="C":
+    print(f'You chose {inp}!')
+else:
+    print("Invalid option!")
