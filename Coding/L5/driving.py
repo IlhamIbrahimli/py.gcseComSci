414a9296a02a0bedc070539f02b52cff35633067
Ilham Ibrahimli <78649705+IlhamIbrahimli@users.noreply.github.com>
2023-11-14 21:37:38 +0000

    driving updated

--- Coding/L5/driving.py
+++ Coding/L5/driving.py
@@ -1,12 +1,14 @@
 age = int(input("Enter Age:"))
-car = input("Do you have a car?(y/n):")
-license = input("Do you have a car?(y/n):")
+car = input("Do you have a car?(y/n):").upper()
+license = input("Do you have a car?(y/n):").upper()
 
 if age >=17 and car=="Y" and license=="Y":
     print("You are good to hit the road!")
+elif age < 17:
+    print("You are gonna have to wait a couple of years!")
 elif car=="N":
     print("You are going to need a car!")
 elif license == "N":
     print("You better take that test!")
-else:
-    print("You are gonna have to wait a couple of years!")
+
+    
